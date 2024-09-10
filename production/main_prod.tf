d02fba21c983e04a42c1485c132bdfe473bbb09e
Chinmay Shastri <chinmay.shastri@digitalturbine.com>
2024-09-10 14:43:04 +0530

    adding terraform modules dir

--- production/main_prod.tf
+++ production/main_prod.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "stagin_group" {
     source = "../modules_main"
     region = var.region
-    cluster_name = production
+    cluster_name = var.cluster_name
     ami = var.ami
     instance_type = var.instance_type
     PATH_TO_PUBLIC_KEY = var.PATH_TO_PUBLIC_KEY
--- staging/main_staging.tf
+++ staging/main_staging.tf
@@ -5,9 +5,9 @@ provider "aws" {
 module "stagin_group" {
     source = "../modules_main"
     region = var.region
-    cluster_name = staging
+    cluster_name = var.cluster_name
     ami = var.ami
-    instance_type = vat.instance_type
+    instance_type = var.instance_type
     PATH_TO_PUBLIC_KEY = var.PATH_TO_PUBLIC_KEY
     min_size = 1
     max_size = 4
